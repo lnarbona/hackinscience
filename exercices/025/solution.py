68ec66196523dbe0f2d825302cf1827a04e46f9b
lnarbona <lara.narbona@cri-paris.org>
2015-11-11 17:16:22 +0100

    025

--- exercices/025/solution.py
+++ exercices/025/solution.py
@@ -1,6 +1,7 @@
 import datetime
-x= datetime.datetime.now().strftime("%y-%m-%d")
-y= datetime.datetime.now().strftime("%H-%M-%S")
+x = datetime.datetime.now().strftime("%y-%m-%d")
+y = datetime.datetime.now().strftime("%H-%M-%S")
 print('Today is ' + x + ' and it is ' + y)
+
 
 
