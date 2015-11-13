a13f8be727fd20c218e177070d4d4539439b3f87
lnarbona <lara.narbona@cri-paris.org>
2015-11-13 15:37:48 +0100

    060

--- exercices/060/solution.py
+++ exercices/060/solution.py
@@ -1,4 +1,4 @@
-alpha = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-for i in alpha:
-    for j in alpha:
+a = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+for i in a:
+    for j in a:
         print(str(i) + str(j))
