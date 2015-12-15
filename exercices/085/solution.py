f16e0e3512526b7be74ef93a7abc9ebfdd4cf109
lnarbona <lara.narbona@cri-paris.org>
2015-12-15 10:40:26 +0100

    085

--- exercices/085/solution.py
+++ exercices/085/solution.py
@@ -1,7 +1,11 @@
 def sort_a_list(l):
     return(sorted(l, reverse=True))
+
+
 def sort_by_mark(my_class):
     return(sorted(my_class, reverse=True))
+
+
 def sort_by_name(my_class):
     from operator import itemgetter
     return(sorted(my_class, key=itemgetter(1)))
